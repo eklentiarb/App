2215a60219f06efbe0c7dca732047402cb11a867
eklentiarb <159138251+eklentiarb@users.noreply.github.com>
2024-03-13 17:52:16 +0300

    Create Panel.js

---
+++ Panel.js
@@ -0,0 +1,22 @@
+const loginForm = document.getElementById('loginForm');
+const message = document.getElementById('message');
+
+const user = {
+  username: 'admin',
+  password: 'emin11'
+};
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === user.username && password === user.password) {
+    message.textContent = 'Login successful! Redirecting...';
+    setTimeout(() => {
+      message.textContent = '';
+      // Redirect to another page or perform some action
+    }, 2000);
+  } else {
+    message.textContent = 'Invalid credentials!';
+  }
+});
